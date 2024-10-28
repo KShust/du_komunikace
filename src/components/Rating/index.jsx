27a7f9bc8e66b5618d434ac426cd270484853653
Kateryna <shust.kateryna0@gmail.com>
2024-10-28 12:06:22 +0100

    prvni ukol

--- src/components/Rating/index.jsx
+++ src/components/Rating/index.jsx
@@ -4,16 +4,20 @@ import './style.css';
 
 export const Rating = () => {
   const [rating, setRating] = useState(0);
+
+  const handleSelect = (value) => {
+    setRating(value)
+  }
   
   return (
     <div className="rating">
       <div className="rating__value">{rating}</div>
       <div className="rating__stars">
-        <Star />
-        <Star />
-        <Star />
-        <Star />
-        <Star />
+        <Star value={1} onSelect={handleSelect}/>
+        <Star value={2} onSelect={handleSelect}/>
+        <Star value={3} onSelect={handleSelect}/>
+        <Star value={4} onSelect={handleSelect}/>
+        <Star value={5} onSelect={handleSelect}/>
       </div>
     </div>
   );
--- src/components/Star/index.jsx
+++ src/components/Star/index.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react';
 import './style.css';
 
-export const Star = () => {
+export const Star = ({ value, onSelect }) => {
   const [glowing, setGlowing] = useState(false);
-  
+  const handleOnClick = () => {
+    setGlowing(true)
+    onSelect(value)
+  }
+
   const starClass = glowing
     ? 'rating__star rating__star--on'
     : 'rating__star';
 
   return (
-    <div className={starClass}></div>
+    <div className={starClass} onClick={handleOnClick}></div>
   );
 };
