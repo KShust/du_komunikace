91abf6629fa671eeab68ab895ab67da2fe310f15
Kateryna <shust.kateryna0@gmail.com>
2024-10-28 12:18:51 +0100

    pridani metody map

--- src/components/Rating/index.jsx
+++ src/components/Rating/index.jsx
@@ -13,11 +13,14 @@ export const Rating = () => {
     <div className="rating">
       <div className="rating__value">{rating}</div>
       <div className="rating__stars">
-        <Star value={1} glowing={rating >= 1} onSelect={handleSelect}/>
-        <Star value={2} glowing={rating >= 2} onSelect={handleSelect}/>
-        <Star value={3} glowing={rating >= 3} onSelect={handleSelect}/>
-        <Star value={4} glowing={rating >= 4} onSelect={handleSelect}/>
-        <Star value={5} glowing={rating >= 5} onSelect={handleSelect}/>
+      {[1, 2, 3, 4, 5].map((v) => (
+          <Star 
+            key={v} 
+            value={v} 
+            glowing={rating >= v}
+            onSelect={handleSelect} 
+          />
+        ))}
       </div>
     </div>
   );
